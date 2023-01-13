dc4463e435949eec540c4502aeedd24d3a7a166f
James Boer <james.boer@gmail.com>
2023-01-13 01:16:45 -0800

    Update gl core profile

--- Source/ImFrame.cpp
+++ Source/ImFrame.cpp
@@ -621,9 +621,10 @@ namespace ImFrame
 		{
 			return 1;
 		}
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		GLFWwindow * window = glfwCreateWindow(s_data->windowWidth, s_data->windowHeight, appName.c_str(), NULL, NULL);
 		if (!window)
 		{
